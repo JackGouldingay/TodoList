b48b7127bc1aca0685f388dafc48c23387643911
JackGouldingay <jackgouldigay@outlook.com>
2024-06-09 22:33:59 +0100

    Added creating of a todo as well as start to a todo viewing.

--- TodoApp/Controllers/NotesController.cs
+++ TodoApp/Controllers/NotesController.cs
@@ -16,6 +16,11 @@ namespace TodoApp.Controllers
 
 		public async Task<IActionResult> Index()
 		{
+			if (!Request.Cookies.ContainsKey("token"))
+			{
+				return Redirect("~/Home");
+			}
+
 			string token = Request.Cookies.First(cookie => cookie.Key == "token").Value;
 			
 			try
@@ -42,6 +47,46 @@ namespace TodoApp.Controllers
 					return View();
 				}
 			}
+
+			return View();
+		}
+
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Create(Note note)
+		{
+            if (!Request.Cookies.ContainsKey("token"))
+            {
+                return Redirect("~/Home");
+            }
+
+			string token = Request.Cookies.First(cookie => cookie.Key == "token").Value;
+            
+
+			try
+			{
+                ApiResponse response = (ApiResponse)await _apiService.GetRequest($"/auth/verify?token={token}");
+                string userId = response.Data.userId;
+				note.OwnerId = userId;
+
+                ApiResponse noteResponse = (ApiResponse)await _apiService.PostRequest("/todo/create", note);
+				dynamic data = noteResponse.Data;
+				ViewData["SuccessMessage"] = noteResponse.Message;
+
+				return Redirect("~/notes");
+			} catch (Exception ex) {
+                if (ex.GetType() == typeof(ApiResponseError))
+                {
+                    ApiResponseError error = (ApiResponseError)ex;
+                    ViewData["Errors"] = new List<string>() { error.ApiMessage };
+
+                    return View();
+                }
+            }
 
 			return View();
 		}
--- TodoApp/Models/Notes/Note.cs
+++ TodoApp/Models/Notes/Note.cs
@@ -5,11 +5,13 @@ namespace TodoApp.Models.Notes
     public class Note
     {
         [JsonPropertyName("id")]
-        public string Id { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
         [JsonPropertyName("name")]
         public string Name { get; set; }
         [JsonPropertyName("description")]
         public string Description { get; set; }
+        [JsonPropertyName("ownerId")]
+        public string OwnerId { get; set; }
         [JsonPropertyName("completed")]
         public Boolean Completed { get; set; }
     }
